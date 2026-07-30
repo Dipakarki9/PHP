36169ee30fb63457ebf0a3bca6ed7d5c333d1e1c
dipakarki9 <dipakarki766@gmail.com>
2026-07-30 20:18:40 +0545

    first commit

---
+++ basic.php
@@ -0,0 +1,30 @@
+<?php
+$age = "20";
+echo "My age is $age";
+
+/*comment in php
+single line comment(//,#)
+*/
+#This is a single line comment
+/*
+this is multiline comment
+*/
+#php variable
+/*$x = 6;
+$y = 7;
+echo $x + $y;
+*/
+
+#two basic way to output data echo or print
+#print "hello";
+
+/*$color = "blue";
+print "my color is $color";
+*/
+#php datatyped to get data type and the value of a variable ,use the var_dump() function.
+$z= 10;
+var_dump($z);
+
+$fruits = "pineaple";
+var_dump($fruits);
+?>
