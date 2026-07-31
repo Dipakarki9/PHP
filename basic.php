ff88686b4fa7ac92d608535d1eae7e9e8d8a04c9
dipakarki9 <dipakarki766@gmail.com>
2026-07-31 12:31:47 +0545

    operators

--- basic.php
+++ basic.php
@@ -27,4 +27,50 @@ var_dump($z);
 
 $fruits = "pineaple";
 var_dump($fruits);
+
+#php operators
+
+/* 1.Arithmetic operators(+,-,%,*,/)
+$x = 10;
+$y = 20;
+$z = $x + $y;
+echo "$z";
+
+$c = 50;
+$e = 40;
+$v = $c % $e;
+echo"$v";
+*/
+
+/* 2.Assignment Operators(=,+=,-=,*=,/=,%=)
+$x = 20;
+$x += 100;
+echo $x;
+*/
+ /* 3.Comparison Opeartors(==,===,!=,>,<,>=,<=,!==)
+$x = 100;
+$y = 100;
+var_dump($x==$y);
+
+$z = 1000;
+$q = "1000";
+var_dump($z === $q);
+*/
+ /* 4. Increment/decrement opeartors.(++,--)
+$x = 9;
+echo ++$x;
+
+*/
+ /* 5. Logical Operator(&&,||,!)
+$x = 1000;
+$y = 50;
+if($x == 1000 && $y == 50){
+    echo "hello";
+}
+    */
+
+# 6. String Opeartors(.,.=)
+$txt = "Hello";
+$txt1 = "world";
+echo $txt . $txt1;
 ?>
